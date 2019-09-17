9f5191478c7e87ef1c4993692830abb968f89dc8
Hari <augten95@gmail.com>
2019-09-17 10:11:47 -0500

    added a semi-colon at the end of the create table query

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
 create database burgers_db;
 use burgers_db;
 create table burgers (
-    id int(12) auto_increment,
+    id int(12) not null auto_increment,
     burger_name varchar(100) not null,
     devoured boolean,
     primary key(id)
-)
+);
