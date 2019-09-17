e3804198f39a789560cf2756c460169ce957fa25
Hari <augten95@gmail.com>
2019-09-17 10:03:14 -0500

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+create database burgers_db;
+use burgers_db;
+create table burgers (
+    id int(12) auto_increment,
+    burger_name varchar(100) not null,
+    devoured boolean,
+    primary key(id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+insert into burgers (burger_name, devoured) values ('black bean burger', false);
+insert into burgers (burger_name, devoured) values ('veggie burger', false);
+insert into burgers (burger_name, devoured) values ('impossible burger', true);
+insert into burgers (burger_name, devoured) values ('avocado burger', false);
+insert into burgers (burger_name, devoured) values ('spicy pepper jack mushroom burger', false);
+insert into burgers (burger_name, devoured) values ('Grilled eggplant burger', false);
+insert into burgers (burger_name, devoured) values ('Cali-Chicken burger', false);
