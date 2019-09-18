355d61c55181233b944c77e3e1b71642240d7d4f
Hari <augten95@gmail.com>
2019-09-18 13:50:24 -0500

    added comments; added code to convert object key/value pairs to SQL syntax; added code for select, insert, and update queries

--- config/orm.js
+++ config/orm.js
@@ -25,21 +25,70 @@ function objToSql(ob) {
         var value = ob[key];
         // check to skip hidden properties
         if(Object.hasOwnProperty.call(ob, key)) {
-            // if string with spaces, add quotations ()
+            // if string with spaces, add quotations (Chickpea burger => 'Chickpea burger')
+            if(typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {burger_name: 'Chickpea burger'} => ["burger_name='Chickpea burger'"]
+            // e.g. {devoured: true} => ["devoured=true"]
+            arr.push(key + "=" + value);
         }
     }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
 }
+
 // Object for all our SQL statement functions.
 var orm = {
-    selectAll: function() {
-        // select * from burgers_db;
+    selectAll: function(tableInput, cb) {
+        var queryString = "select * from " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        });
     },
-    insertOne: function() {
-        // insert into burgers (burger_name, devoured) values (?, ?);
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "insert into " + table;
+        
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "values (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if(err) {
+                throw err;
+            }
+
+            cb(result);
+        });
     },
-    updateOne: function() {
-        // update burgers where id = ?;
+    // An example of objColVals would be {burger_name: Quinoa burger, devoured: true}
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "update " + table;
+        
+        queryString += " set ";
+        queryString += objToSql(objColVals);
+        queryString += " where ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if(err) {
+                throw err;
+            }
+
+            cb(result);
+        });
     }
 };
 
+// Export the orm object for the model (burger.js).
 module.exports = orm;
