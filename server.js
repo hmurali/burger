9f0d1e5d2bdc91603e89815ae4dd3a3e6c7b2865
Hari <augten95@gmail.com>
2019-09-19 16:25:41 -0500

    updated app.use('/api', routes) to app.use('/', routes)

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
-app.use("/api", routes);
+app.use("/", routes);
 
 app.listen(PORT, function() {
     console.log("App now listening at localhost: " + PORT);
