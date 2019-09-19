d0f88aafd7add213c492689b046e6ef798b599c5
Hari <augten95@gmail.com>
2019-09-19 14:34:09 -0500

    added res.redirect('/')

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -41,7 +41,7 @@ router.put("/api/burgers/:id", function(req, res) {
                 return res.status(404).end();
             }
             res.status(200).end(); */
-            res.redirect("/");
+            res.redirect("/");   
         }
     );
 });
